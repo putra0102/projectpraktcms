281c71fd4b09c1f94369cab6e64310a89c37b989
putra0102 <jojosaputra0102@gmail.com>
2025-04-28 00:08:00 +0700

    connect fitur oracle

--- resources/views/Kue/index.blade.php
+++ resources/views/Kue/index.blade.php
@@ -1,18 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Daftar Kue</title>
-</head>
-<body>
-    <h1>Daftar Kue</h1>
-    <ul>
-        @foreach ($kues as $kue)
-            <li>
-                <strong>{{ $kue['title'] }}</strong><br>
-                <a href="{{ url('/kues/' . $kue['id']) }}">Lihat Detail</a>
-            </li>
-            <hr>
+@extends('layouts.app')
+
+@section('title', 'Daftar Kue')
+
+@section('content')
+    <h1 class="mb-4">Daftar Kue</h1>
+    <div class="row">
+        @foreach($kues as $kue)
+        <div class="col-md-4 mb-4">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $kue->nama }}</h5>
+                    <p class="card-text">{{ Str::limit($item->deskripsi, 100) }}</p>
+                    <a href="{{ route('kue.show', $item->id) }}" class="btn btn-primary">Lihat Detail</a>
+                </div>
+            </div>
+        </div>
         @endforeach
-    </ul>
-</body>
-</html>
+    </div>
+@endsection
